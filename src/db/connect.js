a7930efcead5a42337192f55598bcb223c7723e2
emersonnobre <egrnobre@gmail.com>
2023-03-05 15:33:19 -0400

    configuring database connection

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose')
 
-module.exports = (url) => mongoose.connect(url)
+module.exports = (url) => mongoose.connect(url, { 
+    directConnection: true,
+    tls: true,
+    tlsCAFile: 'rds-combined-ca-bundle.pem',
+    tlsAllowInvalidHostnames: true,
+    serverSelectionTimeoutMS: 3000
+})
--- src/validators/user.js
+++ src/validators/user.js
@@ -8,10 +8,12 @@ async function validates(user) {
     const complementaryFields = ['age']
     const invalidFields = fields.filter(field => !requiredFields.includes(field) && !complementaryFields.includes(field))
     const validationErrorsFromModel = await User.validate(user).then(() => null).catch(err => err)
+    
     if (!fields.length) errors.push('You must provide a user field with the properties within')
     if (missingFields.length) errors.push(`Missing fields: ${missingFields}`)
     if (invalidFields.length) errors.push(`Invalid fields for save: ${invalidFields}`)
     if (validationErrorsFromModel) errors.push(validationErrorsFromModel)
+    
     return errors
 }
 
@@ -20,8 +22,10 @@ async function validatesForUpdate(user) {
     const fields = Object.keys(user)
     const acceptedFields = ['name', 'email', 'password', 'age']
     const invalidFields = fields.filter(field => !acceptedFields.includes(field))
+    
     if (!fields.length) errors.push('You must provide a user object with the properties within')
     if (invalidFields.length) errors.push(`Invalid fields for update: ${invalidFields}`)
+    
     return errors
 }
 
